fb1b6098ad86123214d857258f48367cbd30ff15
HugoKeniti <HugoKeniti@192.168.44.210>
2016-04-18 13:10:20 -0300

    Correcao nos loops infinitos. Valor da variavel i foi alterada para possuir o tamanho do vetor

--- test/entidadesTest/RegistroChamadoTest.java
+++ test/entidadesTest/RegistroChamadoTest.java
@@ -5,6 +5,8 @@
  */
 package entidadesTest;
 
+import entidade.Chamado;
+import entidade.RegistroChamado;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -15,8 +17,9 @@ import static org.junit.Assert.*;
 public class RegistroChamadoTest {
     
     @Test
-    public void CadastroChamadoTest() {
-        RegistroChamado chamado = new RegistroChamado(1020,"11h30","12/05/1996","Problema tecnico",);
+    public void RegistroChamadoRedeTest() {
+        Chamado chamado = new Chamado(1020,"Problema","Mistake",10);
+        RegistroChamado chamado = new RegistroChamado(1020,"11h30","12/05/1996","Problema tecnico",chamado);
     }
     
 }
