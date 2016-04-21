2176cb4216302e1adda3be72a454eac712d61881
ta-ma <ta-ma@JonatasPC>
2016-04-21 12:01:09 -0300

    Teste de inserção de empresas

---
+++ test/controleTest/ControleEmpresasTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controleTest;
+
+import Persistencia.EmpresaDAO;
+import controle.ControleEmpresas;
+import entidade.Empresa;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author ta-ma
+ */
+public class ControleEmpresasTest {
+    @Test
+    public void inserirEmpresaTest(){
+        ControleEmpresas controller = new ControleEmpresas();
+        Empresa empresa = new Empresa(12345,"Vivo");
+        controller.inserir(empresa.getNumeroContrato(), empresa.getNomeEmpresa());
+        Empresa empresaInserida = controller.retorna(12345, "Vivo");
+        Assert.assertTrue((empresaInserida.getNomeEmpresa().equals(empresa.getNomeEmpresa())) && (empresaInserida.getNumeroContrato() == empresa.getNumeroContrato()));
+    }
+}
