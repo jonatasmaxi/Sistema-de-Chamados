8fbfaf1a00c2b07e1d9993da0f36ab2df1b03377
31411525 <31411525@089965-P33S12.macklabs.br>
2016-04-25 16:14:20 -0300

    inserção de metodos de teste da classe clienteempresa

---
+++ test/entidadesTest/ClienteEmpresaTest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidadesTest;
+
+import entidade.ClienteEmpresa;
+import entidade.Empresa;
+import entidade.Pessoa;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 31411525
+ */
+public class ClienteEmpresaTest {
+
+    @Test
+    public void cadastrarClienteEmpresatest() {
+        Empresa emp = new Empresa(1000, "Mackenzie");
+        Pessoa p1 = new Pessoa("Hugo", 43569892);
+        ClienteEmpresa ce1 = new ClienteEmpresa(456, emp, 1351848, p1.getNome(), p1.getTelefone());
+        assertEquals((Integer) 456, ce1.getCodigo());
+        assertEquals(1000, emp.getNumeroContrato());
+        assertEquals("Mackenzie", emp.getNomeEmpresa());
+        assertEquals(1351848, ce1.getCpf());
+        assertEquals("Hugo", p1.getNome());
+        assertEquals(43569892, p1.getTelefone());
+    }
+
+    @Test
+    public void editaClienteEmpresaTest() {
+        Empresa emp = new Empresa(1000, "Mackenzie");
+        Pessoa p1 = new Pessoa("Hugo", 43569892);
+        ClienteEmpresa ce1 = new ClienteEmpresa(456, emp, 1351848, p1.getNome(), p1.getTelefone());
+        ce1.setCpf(1236458);
+        ce1.setCodigo(7856);
+        assertNotSame(1351848,ce1.getCpf());
+        assertNotSame(456,ce1.getCodigo());
+        
+    }
+    
+
+}
