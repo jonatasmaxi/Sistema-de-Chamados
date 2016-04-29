ed7591e84c86e22f03dfaaa3e93a22382c97adb4
HugoKeniti <HugoKeniti@10.0.0.6>
2016-04-29 13:45:52 -0300

    Correção de metodos de teste da classe tecnicotest

--- test/entidadesTest/ClienteEmpresaTest.java
+++ test/entidadesTest/ClienteEmpresaTest.java
@@ -36,14 +36,29 @@ public class ClienteEmpresaTest {
 
     @Test
     public void editaClienteEmpresaTest() {
-        Empresa emp = new Empresa(1000, "Mackenzie");
-        Pessoa p1 = new Pessoa("Hugo", 43569892);
-        ClienteEmpresa ce1 = new ClienteEmpresa(456, emp, 1351848, p1.getNome(), p1.getTelefone());
+        Empresa emp2 = new Empresa(1000, "Mackenzie2");
+        Pessoa p2 = new Pessoa("Hugo2", 43569892);
+        ClienteEmpresa ce1 = new ClienteEmpresa(456, emp2, 1351848, p2.getNome(), p2.getTelefone());
         ce1.setCpf(1236458);
         ce1.setCodigo(7856);
         assertNotSame(1351848,ce1.getCpf());
         assertNotSame(456,ce1.getCodigo());
         
+    }
+    
+    @Test(expected = Exception.class)
+    public void verificarSeCodigoDeClienteEmpresaENegativo()throws Exception{
+        Empresa emp3 = new Empresa(1000, "Mackenzie");
+        Pessoa p3 = new Pessoa("Hugo3", 43569892);
+        ClienteEmpresa ce3 = new ClienteEmpresa(-1456, emp3, 1351848, p3.getNome(), p3.getTelefone());
+        
+        
+    }
+    @Test(expected = Exception.class)
+    public void verificarSeCPFDeClienteEmpresaENegativo()throws Exception{
+        Empresa emp4 = new Empresa(41000, "Mackenzie4");
+        Pessoa p4 = new Pessoa("Hugo3", 43569892);
+        ClienteEmpresa ce2 = new ClienteEmpresa(1456, emp4, -1351848, p4.getNome(), p4.getTelefone());
     }
     
 
