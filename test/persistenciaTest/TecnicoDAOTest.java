64e14fbcc84558a29e360754dd00d0b9e8c118b3
41406133 <41406133@099176-P31S301.macklabs.br>
2016-04-29 17:32:04 -0300

    Criação de mais cenários de testes

--- test/persistenciaTest/TecnicoDAOTest.java
+++ test/persistenciaTest/TecnicoDAOTest.java
@@ -9,9 +9,14 @@ import Persistencia.TecnicoDAO;
 import controleTest.ControleTecnicoTest;
 import entidade.Tecnico;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import jdk.jfr.events.FileWriteEvent;
+import org.junit.Assert;
 import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -23,20 +28,20 @@ import org.junit.Test;
  */
 public class TecnicoDAOTest {
 
-
-
     @Test
     public void persistirTecnicoTest() {
         Tecnico tecnico = new Tecnico("Caio", 44516654);
         TecnicoDAO tecnicodao = new TecnicoDAO();
         tecnicodao.put(tecnico);
-        Tecnico inserido = tecnicodao.get(tecnicodao.voltaCashTecnico().size() );
+        Tecnico inserido = tecnicodao.get(tecnicodao.gerarCodigo() - 1);
         assertTrue(((tecnico.getTelefone() == inserido.getTelefone())) && (tecnico.getNome().equals(inserido.getNome())));
     }
-    
-    @Before
-    public void zerarBancoTeste(){
-        
+
+    @Test
+    public void leituraTest() {
+        Tecnico tecnico = new Tecnico("Caio", 44516654);
+        TecnicoDAO tecnicodao = new TecnicoDAO();
+        Assert.assertTrue(tecnicodao.voltaCashTecnico().size() > 0 );
     }
 
 }
